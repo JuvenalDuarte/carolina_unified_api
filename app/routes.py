7047212925f03fe020a7530a12d1b81a9242d206
juvenal.jose <juvenal.jose@totvs.com.br>
2021-09-29 17:16:05 -0300

    fix: adjusting response columns

--- app/routes.py
+++ app/routes.py
@@ -174,7 +174,7 @@ def isRelated(query_ntokens, row, limit):
     return True
 ## TDN SPECIFIC ##
 
-def get_similar_questions(sentence_embeddings_df, query, query_vec, threshold, k, response_columns, id_column="id", validation=False):
+def get_similar_questions(sentence_embeddings_df, query, query_vec, threshold, k, response_columns=None, id_column="id", validation=False):
     global keywordsearch_flag
 
     # KEYWORD SEARCH
@@ -366,11 +366,14 @@ def validate():
         query_vec = model.encode([query], convert_to_tensor=True)
 
         logger.info(f'Validating query {query} against the following articles: {expected_ids}')
+
         results_df, total_matches = get_similar_questions(sentence_embeddings_df=df_tmp, 
                                                         query=query, 
                                                         query_vec=query_vec, 
                                                         threshold=0.0, 
                                                         k=500, 
+                                                        response_columns=None, 
+                                                        id_column="id",
                                                         validation=True)
 
         records_dict_tmp = results_df.to_dict('records')
